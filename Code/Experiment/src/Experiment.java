0a3a886091e49fd3a71696277203bdc620b6d54d
Lecton <lectonlm@gmail.com>
2014-06-28 00:44:55 +0200

    Experiments
    These are the experimental files

--- Code/Experiment/src/Experiment.java
+++ Code/Experiment/src/Experiment.java
@@ -1,26 +1,28 @@
 
+import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
-public class Experiment extends JFrame {
+public class Experiment {
     
     JPanel [] panels;
     JLabel [] labels;
+    JFrame frame;
     public Experiment(){
-        
-        setTitle("Tabbed Pane");
+        frame = new JFrame();
+        frame.setTitle("Tabbed Pane");
+        frame.setMinimumSize(new Dimension(1000, 300));
         panels = new JPanel[5];
         labels = new JLabel[5];
-        
         for(int i = 0; i < 5;i++){
             panels[i] = new JPanel();
             labels[i] = new JLabel();
-            labels[i].setText("You are in area of Tab" + i);
+            labels[i].setText("You are in area of Tab" + i); 
         }
         JTabbedPane jtp = new JTabbedPane();
-        getContentPane().add(jtp);
+        frame.getContentPane().add(jtp);
         
         for(int i = 0; i < 5;i++){
             panels[i].add(labels[i]);
@@ -31,8 +33,8 @@ public class Experiment extends JFrame {
     public static void main(String[] args) {
         
         Experiment tp = new Experiment();
-        tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        tp.setVisible(true);
+        tp.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tp.frame.setVisible(true);
         
     }
 }
