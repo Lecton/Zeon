5837dfb19aea004e9973106f72df3630841f2446
Lecton <lectonlm@gmail.com>
2014-07-28 14:50:41 +0200

    Android

--- Code/Android/Stream2Me/gen/com/example/clientsocket/R.java
+++ Code/Android/Stream2Me/gen/com/example/clientsocket/R.java
@@ -23,12 +23,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080004;
         public static final int button1=0x7f080002;
         public static final int container=0x7f080000;
-        public static final int textView1=0x7f080001;
+        public static final int editPort=0x7f080003;
+        public static final int guiLabel=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
---
+++ Code/Android/Stream2Me/src/client/Connection.java
@@ -0,0 +1,85 @@
+package client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import android.util.Log;
+
+public class Connection {
+    private int PORT;
+    private String address;
+    private Socket soc;
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+    
+    /**
+     * Constructor to assign a default IP address and port number.
+     */
+    public Connection() {
+        PORT =2014;
+        address ="127.0.0.1";
+    }
+    
+    /**
+     * Sets a specific port number for this connection.
+     * @param PORT - the port number to set the original port to.
+     */
+    public void setPORT(int PORT) {
+        this.PORT =PORT;
+    }
+    
+    /**
+     * Sets a specific IP address for this connection.
+     * @param address 
+     */
+    public void setAddress(String address) {
+        this.address =address;
+    }
+    
+    /**
+     * Establishes the connection between a client and the server.
+     * @throws IOException 
+     */
+    public void makeConnection() throws IOException {
+        soc =new Socket(this.address, this.PORT);
+        oos =new ObjectOutputStream(soc.getOutputStream());
+        ois =new ObjectInputStream(soc.getInputStream());
+    }
+    
+    /**
+     * Returns the object output stream and its associated messages for this
+     * connection.
+     * @return
+     * @throws IOException 
+     */
+    public ObjectOutputStream getOutputStream() throws IOException {
+        return oos;
+    }
+    
+    /**
+     * Returns the object input stream and its associated messages for this
+     * connection.
+     * @return
+     * @throws IOException 
+     */
+    public ObjectInputStream getInputStream() throws IOException {
+        return ois;
+    }
+    
+    /**
+     * Writes the message being passed to it via the connection 
+     * by identifying the type and determining the appropriate way of handling
+     * that specific message type.
+     * @param m - message to be sent.
+     */
+    public void write(String m) {
+        try {
+            oos.writeObject(m);
+            oos.flush();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
